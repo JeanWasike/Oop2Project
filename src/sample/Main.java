8831c815ea7cf7b2c37f72338546d5ed6015e23d
KendiN <kendi.njeru@strathmore.edu>
2021-08-31 15:01:08 +0300

    Added a Login frame, Sign u and Owner functionalities.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,11 +10,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
